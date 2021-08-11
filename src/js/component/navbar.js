f861ad52de52641d026c6dd1bdd573305531c270
Mappel21 <Mappel7676@gmail.com>
2021-08-11 14:14:19 +0000

    login to logout

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+	const { store, actions } = useContext(Context);
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
 				<span className="navbar-brand mb-0 h1">Palace: Where Luck Beats Skill</span>
 			</Link>
 			<div className="ml-auto">
-				<Link to="/login">
-					<button className="btn btn-primary">Login</button>
-				</Link>
+				{!store.isLoggedIn ? (
+					<Link to="/login">
+						<button className="btn btn-primary">Login</button>
+					</Link>
+				) : (
+					<Link to="/login">
+						<button
+							className="btn btn-primary"
+							onClick={() => {
+								signOut();
+								actions.updateLogin();
+							}}>
+							Logout
+						</button>
+					</Link>
+				)}
 				&nbsp;
 				<Link to="/register">
 					<button className="btn btn-primary">Register</button>
