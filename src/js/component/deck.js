746a83d0d27912892d4a8dce63290dbe804e5ddf
Sheika1404 <rosheika1404@gmail.com>
2021-08-12 00:29:21 +0000

    added deck

--- src/js/component/deck.js
+++ src/js/component/deck.js
@@ -1,6 +1,16 @@
 let suits = ["spades", "diamonds", "clubs", "hearts"];
 let values = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
 
-const getDeck = () =>{
-    
+export const getDeck = () =>{
+//s=suits v=value
+
+    let deck = [];
+    for (let s=0; s<suits.length; s++){
+        for(let v=0; v<values.length; v++){
+            let card ={Values: values[v], Suit: suits[s]};
+            deck.push(card)
+        }
+        
+    }
+    return deck;
 }
