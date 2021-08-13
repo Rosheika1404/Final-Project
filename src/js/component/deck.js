ebc37f6add97e6755ef52701feaa0fa251d9bcd5
Mappel21 <Mappel7676@gmail.com>
2021-08-13 22:49:44 +0000

    changing suits to symbols

--- src/js/component/deck.js
+++ src/js/component/deck.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-let suits = ["spades", "diamonds", "clubs", "hearts"];
+let suits = ["♠", "♣", "♥", "♦"];
 let values = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
 
 export const getDeck = () =>{
