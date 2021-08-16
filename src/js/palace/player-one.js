bba92ca97a3f53d5d3341a935aeecbc1fd7a1a35
Mappel21 <Mappel7676@gmail.com>
2021-08-16 22:12:14 +0000

    setting up player 1

--- src/js/palace/player-one.js
+++ src/js/palace/player-one.js
@@ -2,5 +2,14 @@ import React from React
 import PropTypes from "prop-types";
 
 export const PlayerOne = () => {
-    
+    <div class="palace-cards">
+        <div class="bottomPalace"></div>
+        <div class="topPalace"></div>
+        <div class="bottomPalace"></div>
+        <div class="topPalace"></div>
+        <div class="bottomPalace"></div>
+        <div class="topPalace"></div>
+    <div class="hand-cards">
+
+    </div>
 }
