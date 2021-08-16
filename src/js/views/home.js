f99b85c04aa785c06002fbcfd8d9e156f4be55d2
Sheika1404 <rosheika1404@gmail.com>
2021-08-16 20:08:00 +0000

    fixed jumbotron

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,6 +3,7 @@ import { Context } from "../store/appContext";
 import { useHistory } from "react-router-dom";
 import Card from "../component/card";
 import { array } from "../component/cardconst";
+import deckHand from "../../img/pexels-kendall.jpg";
 
 import "../../styles/home.scss";
 // import { signIn } from "../component/signIn.js";
@@ -11,32 +12,28 @@ export const Home = () => {
 	const { store } = useContext(Context);
 	return (
 		<div>
-			{/* <div className="jumbotron">
-				<h1 className="display-4">Hello, world!</h1>
-				<p className="lead">
-					This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
-					featured content or information.
-				</p>
-				<hr className="my-4" />
-				<p>
-					It uses utility classes for typography and spacing to space content out within the larger container.
-				</p>
-				<p className="lead">
-					<a className="btn btn-primary btn-lg" href="#" role="button">
-						Learn more
-					</a>
-				</p>
-			</div> */}
-			<div className="jumbotron container">
-				<h1 className="display-4">Palace</h1>
-				<p className="lead">Where Luck Beats Skill</p>
-				<hr className="my-4" />
+			<div
+				className="jumbotron container-fluid bg-dark"
+				style={{
+					backgroundImage: `url(${deckHand})`,
+					backgroundRepeat: "no-repeat",
+					backgroundSize: "cover",
+					width: "90vw",
+					height: "90vh"
+				}}>
+				<h1 className="display-4 justify-content-center text-center">Palace</h1>
+				<p className="lead justify-content-center text-center ">Where Luck Beats Skill</p>
 
-				<p className="lead">
+				{/* <button className="btn btn-primary btn-lg justify-content-center text-center lead">Play Online</button> */}
+
+				{/* <img src={deckHand} className="img-responsive" /> */}
+				{/* <hr className="my-4" /> */}
+
+				{/* <p className="lead">
 					<a className="btn btn-primary btn-lg" href="#" role="button">
 						Learn more
 					</a>
-				</p>
+				</p> */}
 			</div>
 
 			<div className="text-center mt-5 mb-5 row justify-content-center">
