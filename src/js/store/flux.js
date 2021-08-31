9f00371af7ddd13862e970bdd3e47f0aebee39f7
Sheika1404 <rosheika1404@gmail.com>
2021-08-31 22:00:54 +0000

    flux changes

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,5 +1,5 @@
 import firebase from "firebase/app";
-const API_URL = "https://deckofcardsapi.com/api/deck";
+
 
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
@@ -90,31 +90,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 				return myGames;
 			}
 
-			// loadShuffledCards() {
-			// 	fetch(API_URL + "/new/shuffle/")
-			// 		.then(response => response.json())
-			// 		.then(result => {
-			// 			console.log(result),
-			// 				setStore({ deckId: result.deck_id }),
-			// 				console.log("card-id", result.deck_id);
-			// 		})
-			// 		.catch(e => console.error(e));
-			// },
-			// drawCard() {
-			// 	fetch(API_URL + getStore().deckId + "/draw/?count=2", {
-			// 		method: "POST",
-			// 		headers: {
-			// 			"Content-type": "application/json"
-			// 		}
-			// 		// body: "raw",
-			// 		// redirect: "follow"
-			// 	})
-			// 		.then(res => res.json())
-			// 		.then(response => {
-			// 			console.log("response", response);
-			// 			setStore({ drawCard: response });
-			// 		});
-			// }
+			
 		}
 	};
 };
